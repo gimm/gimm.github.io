31802863da408975ec50dbc3ebe6b6b331761184
Gimm Yu <yucc2008@gmail.com>
2014-08-07 02:56:05 +0800

    test plugin with github

--- _plugins/data_generator.rb
+++ _plugins/data_generator.rb
@@ -1,52 +1,47 @@
+require 'fileutils'
+
 module Jekyll
-
-  # Recover from strange exception when starting server without --auto
-  class DataFile < StaticFile
-    
-    def destination(dest)
-      File.join(@base, @dir, @name)
-    end
-
-    def modified?
-      return false
-    end
-
-    def write(dest)
-      return true
-    end
-  end
-
-  class DataGenerator < Generator
+  class DataGenerator < Jekyll::Generator
     safe true
-    priority :highest
-    # Config defaults
-    DIR = '/'
-    POST_LIST_FILENAME = 'data.html'
-    
 
-    # Goes through pages and posts and generates sitemap.xml file
-    #
-    # Returns nothing
+    # Main plugin action, called by Jekyll-core
     def generate(site)
-      dir = File.join(site.source, DIR)
-      filename = POST_LIST_FILENAME
-
-      # Create destination directory if it doesn't exist yet. Otherwise, we cannot write our file there.
-      Dir::mkdir(dir) if !File.directory? dir
-
-      # File I/O: create sitemap.xml file and write out pretty-printed XML
-     
-      file = File.new(File.join(dir, filename), "w")
-      file.write('hello wwww')
-      file.close
-
-      # File.open(File.join(dir, filename), 'w') do |file|
-          # file.write('my file xxx')
-      # end 
-
-      # Keep the sitemap.xml file from being cleaned by Jekyll
-      site.static_files << StaticFile.new(site, site.source, '/', filename)
+      @site = site
+      unless sitemap_exists?
+        write
+        @site.keep_files ||= []
+        @site.keep_files << "data.json"
+      end
     end
 
+
+    # Path to sitemap.xml template file
+    def source_path
+      File.expand_path "data.json", File.dirname(__FILE__)
+    end
+
+    # Destination for sitemap.xml file within the site source directory
+    def destination_path
+      File.expand_path "data.json", @site.dest
+    end
+
+    # copy sitemap template from source to destination
+    def write
+      FileUtils.mkdir_p File.dirname(destination_path)
+      File.open(destination_path, 'w') { |f| f.write(sitemap_content) }
+    end
+
+    def sitemap_content
+      data = Page.new(@site, File.dirname(__FILE__), "", "data.json")
+      data.content = File.read(source_path)
+      data.data["layout"] = nil
+      data.render(Hash.new, @site.site_payload)
+      data.output
+    end
+
+    # Checks if a sitemap already exists in the site source
+    def sitemap_exists?
+      File.exists? File.expand_path "sitemap.xml", @site.source
+    end
   end
 end
