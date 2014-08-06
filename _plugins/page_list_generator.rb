ba965e0b1639ff70307b9a7aac2b6adf4a22270a
Gimm Yu <yucc2008@gmail.com>
2014-08-07 01:30:36 +0800

    test plugin with github

--- _plugins/page_list_generator.rb
+++ _plugins/page_list_generator.rb
@@ -1,49 +1,8 @@
 module Jekyll
 
-  class Post
-    attr_accessor :name
-
-    def full_path_to_source
-      File.join(@base, @name)
-    end
-    
-    def path_to_source
-      File.join(@name)
-    end
-
-    def location_on_server(my_url)
-      "#{my_url}#{url}"
-    end
-  end
-
-  class Page
-    attr_accessor :name
-
-    def full_path_to_source
-      File.join(@base, @dir, @name)
-    end
-    
-    def path_to_source
-      File.join(@dir, @name)
-    end
-
-    def location_on_server(my_url)
-      location = "#{my_url}#{url}"
-      location.gsub(/index.html$/, "")
-    end
-  end
-
-
-  class Layout
-    def full_path_to_source
-      File.join(@base, @name)
-    end
-  end
-
   # Recover from strange exception when starting server without --auto
   class SitemapFile < StaticFile
-    require 'set'
-
+    
     def destination(dest)
       File.join(@base, @dir, @name)
     end
@@ -60,7 +19,7 @@ module Jekyll
   class SitemapGenerator < Generator
 
     # Config defaults
-    DIR = 'data'
+    DIR = '/'
     POST_LIST_FILENAME = 'data.html'
     
 
@@ -85,40 +44,5 @@ module Jekyll
       site.static_files << Jekyll::SitemapFile.new(site, dir, '/', filename)
     end
 
-
-
-    
-
-    
-    # Is the page or post listed as something we want to exclude?
-    #
-    # Returns boolean
-    def excluded?(site, name)
-      @config['exclude'].include? name
-    end
-
-    def posts_included?(site, name)
-      @config['include_posts'].include? name
-    end
-
-    # Is the change frequency value provided valid according to the spec
-    #
-    # Returns boolean
-    def valid_change_frequency?(change_frequency)
-      VALID_CHANGE_FREQUENCY_VALUES.include? change_frequency
-    end
-
-    # Is the priority value provided valid according to the spec
-    #
-    # Returns boolean
-    def valid_priority?(priority)
-      begin
-        priority_val = Float(priority)
-        return true if priority_val >= 0.0 and priority_val <= 1.0
-      rescue ArgumentError
-      end
-
-      false
-    end
   end
 end
