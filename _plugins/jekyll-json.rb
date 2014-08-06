6cc87c565f8946a8d0e7ea8e3a37e9d8f0c4cd85
Gimm Yu <yucc2008@gmail.com>
2014-08-07 03:36:13 +0800

    test plugin with github

--- _plugins/jekyll-json.rb
+++ _plugins/jekyll-json.rb
@@ -3,26 +3,28 @@ require 'fileutils'
 module Jekyll
   class JekyllJson < Jekyll::Generator
     safe true
-
+    priority = :low
+    
+    FILENAME = "data.txt"
     # Main plugin action, called by Jekyll-core
     def generate(site)
       @site = site
       unless data_exists?
         write
         @site.keep_files ||= []
-        @site.keep_files << "data.json"
+        @site.keep_files << FILENAME
       end
     end
 
 
     # Path to sitemap.xml template file
     def source_path
-      File.expand_path "data.json", File.dirname(__FILE__)
+      File.expand_path FILENAME, File.dirname(__FILE__)
     end
 
     # Destination for sitemap.xml file within the site source directory
     def destination_path
-      File.expand_path "data.json", @site.dest
+      File.expand_path FILENAME, @site.dest
     end
 
     # copy sitemap template from source to destination
@@ -32,7 +34,7 @@ module Jekyll
     end
 
     def sitemap_content
-      data = Page.new(@site, File.dirname(__FILE__), "", "data.json")
+      data = Page.new(@site, File.dirname(__FILE__), "", FILENAME)
       data.content = File.read(source_path)
       data.data["layout"] = nil
       data.render(Hash.new, @site.site_payload)
@@ -41,7 +43,7 @@ module Jekyll
 
     # Checks if a sitemap already exists in the site source
     def data_exists?
-      File.exists? File.expand_path "data.json", @site.source
+      File.exists? File.expand_path FILENAME, @site.source
     end
   end
 end
