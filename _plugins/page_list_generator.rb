8892ac8add1eb5978eeba8933ae9872145d6083f
Gimm Yu <yucc2008@gmail.com>
2014-08-07 00:09:35 +0800

    test plugin with github

--- _plugins/page_list_generator.rb
+++ _plugins/page_list_generator.rb
@@ -1,24 +1,45 @@
+require 'json'
+
 module Jekyll
 
-  class ListPage < Page
-    def initialize(site, base, dir, list)
-      @site = site
-      @base = base
-      @dir = dir
-      @name = 'post.list.json'
-
-      self.process(@name)
-      self.read_yaml(File.join(base, '_layouts'), 'post.list.html')
-      self.data['list'] = list
-
-    end
-  end
-
-  class PageListGenerator < Generator
+  class DataGenerator < Generator
 
     def generate(site)
-      site.pages << ListPage.new(site, site.source, 'data', 'list data')
+      @site = site
+
+      process_post_list
+    end
+
+    def process_post_list
+
+            generate_aliases()
+  
+    end
+
+    def generate_aliases()
+        dir = File.join(@site.dest, 'data')
+        puts File.join(dir ,'test.json')
+        txt = File.open(File.join(dir ,'test.json'), 'w+')
+        txt.puts("hello world")
+        
+        @site.static_files << Jekyll::JsonDataFile.new(@site, @site.dest, 'data' ,'test.json')
+
     end
   end
 
+  class JsonDataFile < StaticFile
+    require 'set'
+
+    def destination(dest)
+      File.join(dest, @dir, @name)
+    end
+
+    def modified?
+      return false
+    end
+
+    def write(dest)
+      return true
+    end
+  end
 end
