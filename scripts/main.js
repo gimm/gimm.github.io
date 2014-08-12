4364105523d51a087e0dfb77ca76efcef71b2dbc
Gimm Yu <yucc2008@gmail.com>
2014-08-12 11:46:35 +0800

    fix disqus

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,14 @@
-var baseurl = location.href.split("#").shift();
-var cache = {};
-var loading = false;
-var mainContainer = "main.container";
-var dataWrapper = "#data";
-var articleListWrapper = ".articles";
-var articleListTail = ".articles-tail";
-var articleListTailLink = articleListTail + " > .next-page";
 $(function () {
-    console.log("ready!");
-    var container = $("main.container");
+    var baseurl = location.href.split("#").shift();
     var hash = window.location.hash.replace(/#\/?/, "");
-
+    var cache = {};
+    var loading = false;
+    var mainContainer = "main.container";
+    var dataWrapper = "#data";
+    var articleListWrapper = ".articles";
+    var articleListTail = ".articles-tail";
+    var articleListTailLink = articleListTail + " > .next-page";
+    var container = $("main.container");
     cache[baseurl] = container.html();
 
     //store index page to history
@@ -32,7 +30,6 @@ $(function () {
      *   bind events
      */
     $(document).on("click", "a[href^='/']", function (e) {
-        console.log("html 5 click");
         var url = this.href;
         if (!this.target && (url.indexOf("#") === -1)) {
             switchPage(url);
@@ -42,8 +39,6 @@ $(function () {
     });
 
     window.onpopstate = function (e) {
-        console.log("restore AT ", location.href, history.state);
-
         history.state && container.html(history.state.content);
         e.preventDefault();
     };
@@ -72,67 +67,91 @@ $(function () {
         }
         lastScrollTop = window.scrollY;
     };
-});
 
-//load pages/posts, switch views
-function switchPage(url) {
-    var container = $(mainContainer);
-    if (url === location.href.split("#").shift()) {
-        console.log("skip");
-        container.html(history.state.content);
-    } else if (cache[url]) {
-        console.log("cache");
-        container.html(cache[url]);
-        window.history.replaceState({"content": cache[url]}, "gimm", url);
-    } else {
+
+    //load pages/posts, switch views
+    function switchPage(url) {
+        var container = $(mainContainer);
+        if (url === location.href.split("#").shift()) {
+            window.scrollTo(0, 0);
+            container.html(history.state.content);
+        } else if (cache[url]) {
+            window.scrollTo(0, 0);
+            container.html(cache[url]);
+            window.history.replaceState({"content": cache[url]}, "gimm", url);
+        } else {
+            load(url, function (res, status) {
+                if (status === "success") {
+                    cache[url] = res;
+                    cache[url] = $("<div>").append(res).find(dataWrapper).html();
+                    window.scrollTo(0, 0);
+                    container.html(cache[url]);
+                    console.log("push", url);
+                    window.history.pushState({"content": cache[url]}, "gimm", url);
+                } else {
+                    container.html("Error loading resource", url);
+                }
+            });
+        }
+
+    }
+
+    //load more posts, incremental loading of the post list
+    function nextPosts() {
+        var url = $(articleListTailLink).get(0).href;
+        $(articleListTailLink).html("Loading ...");
         load(url, function (res, status) {
             if (status === "success") {
                 cache[url] = res;
-                cache[url] = $("<div>").append(res).find(dataWrapper).html();
-                container.html(cache[url]);
-                console.log("push", url);
-                window.history.pushState({"content": cache[url]}, "gimm", url);
+                cache[url] = $("<div>").append(res).find(mainContainer);
+                $(articleListWrapper).append(cache[url].find(articleListWrapper + " > .article").addClass("newly-load"));
+                $(articleListTail).html(cache[url].find(articleListTail));
             } else {
-                container.html("Error loading resource", url);
+                $(articleListTailLink).html("Error loading articles!");
             }
+            loading = false;
         });
     }
-
-}
-
-//load more posts, incremental loading of the post list
-function nextPosts() {
-    var url = $(articleListTailLink).get(0).href;
-    $(articleListTailLink).html("Loading ...");
-    load(url, function (res, status) {
-        if (status === "success") {
+    // load html resource by url
+    function load(url, callback) {
+        $.get(url, function (res, status) {
             cache[url] = res;
-            cache[url] = $("<div>").append(res).find(mainContainer);
-            $(articleListWrapper).append(cache[url].find(articleListWrapper + " > .article"));
-            $(articleListTail).html(cache[url].find(articleListTail));
+            $.isFunction(callback) && callback.apply(null, arguments);
+        });
+    };
+
+
+    function isElementInViewport(el) {
+        var rect = el.getBoundingClientRect();
+
+        return rect.bottom > 0 &&
+            rect.right > 0 &&
+            rect.left < (window.innerWidth || document. documentElement.clientWidth)  &&
+            rect.top < (window.innerHeight || document. documentElement.clientHeight);
+    }
+
+
+
+    $(document).on("click", ".show-comment-link", function () {
+        $(this).fadeOut();
+        if (window.DISQUS) {
+
+            DISQUS.reset({
+                reload: true,
+                config: function () {
+                    this.page.identifier = location.pathname;
+                    this.page.url = location.href;
+                }
+            });
+
         } else {
-            $(articleListTailLink).html("Error loading articles!");
+
+            $.ajax({
+                type: "GET",
+                url: "http://gimm-github-io.disqus.com/embed.js",
+                dataType: "script",
+                cache: true
+            });
         }
-        loading = false;
     });
-}
-// load html resource by url
-function load(url, callback) {
-
-    console.log("load", url);
-    $.get(url, function (res, status) {
-        cache[url] = res;
-        $.isFunction(callback) && callback.apply(null, arguments);
-    });
-};
-
-
-function isElementInViewport(el) {
-    var rect = el.getBoundingClientRect();
-
-    return rect.bottom > 0 &&
-        rect.right > 0 &&
-        rect.left < (window.innerWidth || document. documentElement.clientWidth)  &&
-        rect.top < (window.innerHeight || document. documentElement.clientHeight);
-}
-
+});
